5bee9492768152fa1c800f9b8692dcddb42bd9ac
lijun <1156110902@qq.com>
2017-05-02 19:16:09 +0800

    MySQL

--- UseUser.py
+++ UseUser.py
@@ -2,7 +2,7 @@ import orm
 from models import User,Blog,Comment
 
 def test():
-    yield from orm.create_pool(user='www-data',passwd='www-data',database='awesome')
+    yield from orm.create_pool(user='www-data',passwd='www-data',databases='awesome')
 
     a = User(name='Test',email='test@example.com',passwd='1234567890',image='about :blank')
 
