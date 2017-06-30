0560a3b10e2f9fed2129d9de2e9d23b02263e749
lijun <1156110902@qq.com>
2017-06-30 09:28:09 +0800

    Algorithms

--- cookieLoad.py
+++
@@ -1,9 +0,0 @@
-import cookielib
-import urllib2
-
-cookie = cookielib.MozillaCookieJar()
-cookie.load('cookie.txt',ignore_discard = True,ignore_expires= True)
-request = urllib2.Request("https://www.baidu.com")
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-response = opener.open(request)
-print response.read()
--- demo.py
+++
@@ -1,4 +0,0 @@
-import urllib2
-
-response = urllib2.urlopen("https://www.baidu.com")
-print response.read()
